61647da1d056d106e7605c1c50070727ea918073
Anupriya1500 <raghuwanshianupriya8@gmail.com>
2020-04-10 19:13:23 +0530

    Adding day1 file of forsk

---
+++ day1.py
@@ -0,0 +1,82 @@
+
+print("hello from anupriya")
+print(2+3)
+
+
+import math
+print(math.sqrt(2))
+math.sqrt(2)
+math.pow(2,3)
+
+
+input("enter the name: ")
+myname =input("enter the name: ")
+print(myname)
+
+age=20.5
+name="anu"
+
+flag=True
+type(flag)
+
+value=None
+type(value)
+a = None
+type(a)
+
+age=input("enter the age: ")
+
+"fork" - str
+'forl' - str
+"f" - str
+'f'- str
+
+int(2.3)
+float(3)
+
+age =int(age)
+type(age)
+
+str1="hey"
+len(str1)
+
+# single line comment
+"""
+multi line comment 
+xyubxu
+"""
+
+name=input("enter the name: ")
+print(len(name))
+
+# indexing or say slicing in python
+name[0]
+name[6]
+name[0:5]
+name[-2]
+
+name[0:6:2]
+name[0:9:1]
+name[0:9:3]
+name[1:]
+name[:]
+name
+name[::2] # no start or end specified 
+name[::-1] # reverses the string
+
+str1 = "corona virus"
+str1[0:6]
+str1[-9]
+str1[-15]
+str1[:::]
+str1[::]
+str1[:]
+str1
+
+#######
+a = ['a', 'b']
+type(a)
+a += [1, 2, 3]
+a
+
+##########
